109888f3ee3697a43f6a105174f8b258dc3dc9bd
Will Diable <wtdiable@icloud.com>
2022-01-18 16:00:29 +0000

    testing bike class

--- Lib/DockingStation.rb
+++ Lib/DockingStation.rb
@@ -1,5 +1,18 @@
 class DockingStation
   def release_bike
-    return "Bike released"
+    # return "Bike released"
+    $bike += 1
   end
 end
+
+class Bike
+  def initialize
+    working_bike += 1
+  end
+end
+
+# docking_station = DockingStation.new
+# docking_station
+# docking_station.release_bike
+# puts $bike
+# p $bike.working
---
+++ Spec/Bike_spec.rb
@@ -0,0 +1,12 @@
+require './Lib/DockingStation.rb'
+describe Bike do 
+  it 'stores bike health' do 
+  #Arrange
+    my_bike = Bike.new
+  #Act
+    # expect(docking_station.release_bike).to eq "Bike Released"
+    # docking_station.respond_to?release_bike
+    # respond_to?(docking_station.release_bike) 
+  #Assert
+  end
+end
