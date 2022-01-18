63ad84c351099d732d481911bd010e39e5de7f7b
benfielding74 <billy.fadden@live.com>
2022-01-18 14:55:51 +0000

    Fixed indentation on spec file

--- Spec/DockingStation_spec.rb
+++ Spec/DockingStation_spec.rb
@@ -1,11 +1,10 @@
-require './DockingStation.rb'
+require './Lib/DockingStation.rb'
 describe DockingStation do 
   it 'stores and releases bikes' do 
   #Arrange
     docking_station = DockingStation.new
   #Act
-    expect(docking_station.release_bike).to eq
+    expect(docking_station.release_bike).to eq "Bike Released"
   #Assert
-
   end
 end
